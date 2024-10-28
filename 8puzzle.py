9919c77fc37309d9cb1d7b514d437ab49ada146c
Amitaisela <68296620+Amitaisela@users.noreply.github.com>
2024-10-28 20:36:11 +0200

    TODO

---
+++ 8puzzle.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+import numpy as np
+
+
+def count_inversions(state):
+    inv_count = 0
+    array = [tile for tile in state if tile != 0]
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i] > array[j]:
+                inv_count += 1
+    return inv_count
+
+
+def is_solvable(state):
+    inv_count = count_inversions(state)
+    return inv_count % 2 == 0
+
+
+def generate_solvable_8_puzzles():
+    target_state = (1, 2, 3, 4, 5, 6, 7, 8, 0)
+    all_states = set()
+    for perm in permutations(target_state):
+        if is_solvable(perm):
+            all_states.add(perm)
+    return all_states
+
+
+# Get all solvable 8-puzzles
+solvable_puzzles = generate_solvable_8_puzzles()
+print(f"Total solvable 8-puzzle configurations: {len(solvable_puzzles)}")
---
+++ Heuristic.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+
+class Heuristic:
+    # euclidian distance
+    @staticmethod
+    def euclidean(node, goal):
+        return sqrt((node[0] - goal[0])**2 + (node[1] - goal[1])**2)
+
+    # manhattan distance
+    @staticmethod
+    def manhattan(node, goal):
+        return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+
+    # To do the same as the paper, we need to calculate:
+    # c*(huristic+
